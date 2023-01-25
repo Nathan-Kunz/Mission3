a1b1fddaece33f64587d4f92c0758138fddb577b
korbin-derr <korbinderr@gmail.com>
2023-01-25 09:39:14 -0700

    updates to program

--- csharpPracticeFun/Program.cs
+++ csharpPracticeFun/Program.cs
@@ -6,56 +6,74 @@ namespace csharpPracticeFun
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Tic Tac Toe!!");
-            string[,] board  = new string [3,3];
-            for( int i = 0; i<3; i++)
-            {
-                for (int iin = 0; iin<3; iin++)
+            string playAgain = "y";
+            while (playAgain == "y"){
+
+                Console.WriteLine("Welcome to Tic Tac Toe!!");
+                string[,] board = new string[3, 3];
+                for (int x = 0; x < 3; x++)
                 {
-                    board[i, iin] = "_";
-                    Console.Write(board[i, iin] + " ");
+                    for (int iin = 0; iin < 3; iin++)
+                    {
+                        board[x, iin] = "_";
+                        Console.Write(board[x, iin] + " ");
+                    }
+                    Console.WriteLine();
                 }
-                Console.WriteLine();
-            }
             ;
-            string player;
-            for (int i = 1; i <= 9; i++)
-            {
-                if(i % 2 ==0)
+                string player = "";
+                bool winner = false;
+                int i = 1;
+                //for (int i = 1; i <= 9; i++)
+                while (i < 10 || winner == false)
                 {
-                     player = "O";
+                    if (i % 2 == 0)
+                    {
+                        player = "O";
+                    }
+                    else
+                    {
+                        player = "X";
+                    }
+
+                    Console.WriteLine("Player " + player + " it's your turn");
+                    Console.WriteLine("What Row do you want?");
+                    int col = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("What Column do you want?");
+                    int row = Convert.ToInt32(Console.ReadLine());
+
+                    board[col - 1, row - 1] = player;
+                    Support support = new Support();
+                    support.printBoard(board);
+                    support.winnerCheck(board);
+                    i++;
+
+                    //for (int x = 0; x < 3; x++)
+                    //{
+                    //    for (int iin = 0; iin < 3; iin++)
+                    //    {
+
+                    //        Console.Write(board[x, iin] + " ");
+                    //    }
+                    //    Console.WriteLine();
+                    //}
                 }
-                else
+
+                if (i == 9 && winner == false)
                 {
-                     player = "X";
+                    Console.WriteLine("It's a DRAW!");
                 }
-                
-                Console.WriteLine("Player " + player + " it's your turn");
-                Console.WriteLine("What Row do you want?");
-                int col = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine("What Column do you want?");
-                int row = Convert.ToInt32(Console.ReadLine());
-
-                board[col - 1, row - 1] = player;
-                Support support = new Support();
-                support.printBoard(board);
-
-                
-                //for (int x = 0; x < 3; x++)
+                //else
                 //{
-                //    for (int iin = 0; iin < 3; iin++)
-                //    {
-
-                //        Console.Write(board[x, iin] + " ");
-                //    }
-                //    Console.WriteLine();
+                //    Console.WriteLine(player + "WINS!!!");
                 //}
-            }
-               
-            
 
-            
+                Console.Write("Play again? Y/N:  ");
+                playAgain = Console.ReadLine().ToLower();
+
+            }
+            Console.WriteLine("Thanks for playing!!");
         }
         
     }
